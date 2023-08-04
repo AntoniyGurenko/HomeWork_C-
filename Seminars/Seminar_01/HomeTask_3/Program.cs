5ac31065a05c542e165f6e3218afe10f2a00f4c3
antongurenko <gurenko.01@gmail.com>
2023-08-04 22:47:01 +0600

    complete_task_3

---
+++ Seminars/Seminar_01/HomeTask_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 6. Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли это число чётным (делится ли оно на 2 без остатка).
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+bool even = true;
+
+if (number % 2 == 0)
+{
+    even = true;
+}
+else
+{
+    even = false;
+}
+
+if (even == true)
+{
+    Console.WriteLine($"Число {number} является чётным");
+}
+else
+{
+    Console.WriteLine($"Число {number} является не чётным");
+}
