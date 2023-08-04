f64a2fe174e4ec884d98ebbff1629504f9cb4b08
antongurenko <gurenko.01@gmail.com>
2023-08-04 22:10:31 +0600

    initial_commit

---
+++ Seminars/Seminar_01/HomeTask_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine()!);
+int max = 0;
+int min = 0;
+
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
+else
+{
+    max = number2;
+    min = number1;
+}Console.WriteLine($"Число {max} больше числа {min}.");
