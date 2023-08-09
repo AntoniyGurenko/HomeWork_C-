d6f1c061057486be7452e1b93609ad286f96e845
antongurenko <gurenko.01@gmail.com>
2023-08-09 18:54:52 +0600

    create_seminar2_and_complete_Home_Task_1

---
+++ Seminars/Seminar_02/HomeTask_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// int randNumb = new Random().Next(100, 1000);
+
+// int secondDig = randNumb / 10 % 10;
+
+// System.Console.WriteLine($"{randNumb} - > {secondDig}");
+System.Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int secondDig = 0;
+
+if (number < 100) System.Console.WriteLine("Ошибка, не выполнены условия задачи");
+else if (number > 999) System.Console.WriteLine("Ошибка, не выполнены условия задачи");
+else 
+{
+    secondDig = number / 10 % 10;
+    System.Console.WriteLine($"{number} - > {secondDig}");
+}
