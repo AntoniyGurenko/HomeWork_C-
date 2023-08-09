b603e3248fb2486a3aa65560766b90b16b02a38a
antongurenko <gurenko.01@gmail.com>
2023-08-09 19:44:47 +0600

    complete_task_2

---
+++ Seminars/Seminar_02/HomeTask_2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine()!);
+
+if (number / 100 == 0)
+{
+    System.Console.WriteLine("У числа нет третьей цифры");
+}
+else
+{
+    while (number / 1000 != 0)
+    {
+        number = number / 10;
+    }
+
+    number = number % 10;
+    System.Console.WriteLine();
+    Console.WriteLine($"Третье число - {number}");
+}
+
