192793d82798daecd7c9320ac876eb493dc43630
antongurenko <gurenko.01@gmail.com>
2023-08-10 10:59:00 +0600

    complete_HomeTask3

--- Seminars/Seminar_02/HomeTask_3/Program.cs
+++ Seminars/Seminar_02/HomeTask_3/Program.cs
@@ -7,3 +7,25 @@
 // 7 -> да
 // 1 -> нет
 
+System.Console.WriteLine("Введите число дня недели, чтобы узнать выходной это или нет");
+int number = int.Parse(Console.ReadLine()!);
+
+switch (number)
+{
+    case 1: System.Console.WriteLine($"{number} -> нет");
+    break;
+    case 2: System.Console.WriteLine($"{number} -> нет");
+    break;
+    case 3: System.Console.WriteLine($"{number} -> нет");
+    break;
+    case 4: System.Console.WriteLine($"{number} -> нет");
+    break;
+    case 5: System.Console.WriteLine($"{number} -> нет");
+    break;
+    case 6: System.Console.WriteLine($"{number} -> да");
+    break;
+    case 7: System.Console.WriteLine($"{number} -> да");
+    break;
+    default:System.Console.WriteLine("Такого дня недели нет");
+    break;
+}
