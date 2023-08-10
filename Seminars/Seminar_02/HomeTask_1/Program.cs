a3e8e8348cd58f7847b60d5de82c02fca75759f3
antongurenko <gurenko.01@gmail.com>
2023-08-10 10:51:26 +0600

    added_HomeTask3_and_suplemended_tasks_with_conditions

--- Seminars/Seminar_02/HomeTask_1/Program.cs
+++ Seminars/Seminar_02/HomeTask_1/Program.cs
@@ -1,9 +1,15 @@
-﻿// int randNumb = new Random().Next(100, 1000);
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+//  вторую цифру этого числа. 456 -> 5, 782 -> 8
+
+
+// int randNumb = new Random().Next(100, 1000); // С рандомным числом
 
 // int secondDig = randNumb / 10 % 10;
 
 // System.Console.WriteLine($"{randNumb} - > {secondDig}");
-System.Console.WriteLine("Введите трёхзначное число");
+
+
+System.Console.WriteLine("Введите трёхзначное число"); // С числом введённым пользователем
 int number = int.Parse(Console.ReadLine()!);
 int secondDig = 0;
 
